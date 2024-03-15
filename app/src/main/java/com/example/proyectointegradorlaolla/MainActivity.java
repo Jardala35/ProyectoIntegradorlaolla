f7c79a8ccb5cc6601597e88c343da73123bb3125
Pablo Navarro <pablonavarroduro@gmail.com>
2024-03-16 00:50:06 +0100

    dos

--- app/src/main/java/com/example/proyectointegradorlaolla/MainActivity.java
+++ app/src/main/java/com/example/proyectointegradorlaolla/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.proyectointegradorlaolla;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -16,16 +14,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.bumptech.glide.Glide;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 
@@ -59,9 +53,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         this.datos = new ArrayList<>();
         cargarDatos(datos);
-       // datos.add(new ItemLista(R.drawable.abejaruco, "Abejaruco"));
-       // datos.add(new ItemLista(R.drawable.abejaruco, "Abejaruco2"));
-        //datos.add(new ItemLista(R.drawable.abejaruco, "Abejaruco3"));
         miRecyclerView = findViewById(R.id.recview);
         miRecyclerView.setHasFixedSize(true);
         miLayoutManager = new LinearLayoutManager(this);
